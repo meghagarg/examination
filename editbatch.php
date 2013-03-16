0f20c939491c9ecdd1584ead5106b05d520d6eb0
root <root@megha-desktop.(none)>
2013-03-16 13:54:22 +0530

    ed

--- editbatch.php
+++ editbatch.php
@@ -22,7 +22,14 @@ $tname="Manish Nautiyal";
             <form method="GET">
 
                 Batch Id&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value="<?php echo $batch_id;?>" name="bbid"><br>
-             Trainer name<input type="text" value="<?php echo $tname;?>" name="ttname"><br>
+
+Trainer Name <select name='ttname'>
+           <option value='-1'><?php echo $tname;?></option>
+        <option value="1">Megha Garg</option>
+        <option value="2">Manish Nautiyal</option>
+        <option value="3">Manish Saini</option>
+
+</select>
 
                        <input type="submit" name="submit" value="edit">
 
